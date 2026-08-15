09ea5a81651970fad882d5557332712a47bae9d5
XianZS <17791630864@163.com>
2026-08-15 22:17:20 +0800

    枚举&字符串

--- c_plus_11/3.cpp
+++ c_plus_11/3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #define MAXSIZE 100
 using namespace std;
 
@@ -63,15 +65,54 @@ void show_const_var()
     cout << name << endl;
 }
 
+void show_var_type_transfer()
+{
+    float num{1.23};
+    int number = int(num);
+    cout << number << endl;
+}
+
+void show_string()
+{
+    string nums{"My name is jom"};
+    cout << "[nums] >>> " << nums << endl;
+    cout << "[nums_3] >>> " << nums[3] << endl;
+    // C++11 新增特性-原始字符串
+    // 被关键字“R(字符串)”标识的字符串之中的所有特殊字符都不再生效，比如\n。
+    cout << R"(\t\t\t)" << endl;
+}
+
+struct inflatable
+{
+    /* data */
+    char name[20];
+    float volume;
+    double price;
+};
+
+void show_struct()
+{
+    inflatable inf{"披萨", 2.2, 15.68};
+    cout << inf.name << endl;
+    cout << inf.volume << endl;
+    cout << inf.price << endl;
+    inflatable infs[2]{{"披萨", 2.2, 15.68},
+                       {"饼子", 1.0, 3.5}};
+    cout << infs[1].name << endl;
+}
+
 int main()
 {
-    show_short_int_long_var();
-    show_size_of();
-    pr_max_size();
-    init_var();
-    show_unsigned_var();
-    show_char_var();
-    show_bool_var();
-    show_const_var();
+    // show_short_int_long_var();
+    // show_size_of();
+    // pr_max_size();
+    // init_var();
+    // show_unsigned_var();
+    // show_char_var();
+    // show_bool_var();
+    // show_const_var();
+    // show_var_type_transfer();
+    // show_string();
+    // show_struct();
     return 0;
 }
